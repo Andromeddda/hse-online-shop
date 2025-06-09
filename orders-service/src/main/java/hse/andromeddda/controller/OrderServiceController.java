68f933f9e7e9e2c30f3b50fed3fa02e0486d9b40
Andromeddda <andrey.shik2005@gmail.com>
2025-06-09 16:06:02 +0300

    Add controllers and readme

---
+++ orders-service/src/main/java/hse/andromeddda/controller/OrderServiceController.java
@@ -0,0 +1,12 @@
+package hse.andromeddda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderServiceController
+{
+
+}
